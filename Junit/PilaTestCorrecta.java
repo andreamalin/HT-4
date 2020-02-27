f796fca5f7c3f0c9c12ddd6ee5d778787e8602aa
Carlos21122000 <53351491+Carlos21122000@users.noreply.github.com>
2020-02-26 20:56:53 -0600

    Add files via upload

---
+++ Junit/PilaTestCorrecta.java
@@ -0,0 +1,48 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**********************************************************
+*pilatestCorrecta.java				Fecha de creacion: 01/01/2020
+*							Ultima fecha de modificacion: 24/02/2020
+*							
+*
+*
+*@author Andrea Amaya #19357
+*@author Carlos Raxtum #19721
+**********************************************************/
+class PilaTestCorrecta {
+
+	@Test
+	void test() {
+		Calculadora prueba = new Calculadora();
+		iStack<Integer> stack = new ArrayLists(); 
+		
+		String[] opc = { "5", "5", "+" };
+		int num;
+		int res=0;
+		int val2, val1; //varibles para operar
+		
+		for (int i=0; i<opc.length; i++) {
+			try {
+				num =Integer.parseInt(opc[i]);
+				stack.push(num);
+			} catch (Exception e){
+				//obtienen los dos ultimos numeros para la operacion
+				val1 = stack.pop();
+				val2 = stack.pop();
+				
+				switch(opc[i]){
+				case "+":
+					res = prueba.sumar(val2, val1);
+					break;
+			}
+			stack.push(res); 
+			} 
+		}
+		
+		int resFinal = stack.peek();
+		System.out.println(resFinal);
+		assertEquals(10, resFinal);
+	}
+
+}
